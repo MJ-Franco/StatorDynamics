9da590856aa898f7fd63c4bb07968e799730db3b
M-J Franco-Oñate <mjfranco2009@gmail.com>
2022-06-16 10:00:44 +0200

    Main
    Traces speed rate model.

---
+++ Fortran/KMC/Speed rate/TracesKMC_Berg.f90
@@ -0,0 +1,130 @@
+!Program for calculate the curves of recruitment and stall from the
+!bacterial flagellar motor with Kinetic Monte Carlo
+
+!29 march 2021
+
+!María-José Franco Oñate
+
+include 'BergKMC.f90'
+
+
+use KMC_T
+implicit real*8 (a-h,o-z)
+
+integer, parameter :: n_v=13, n_sim=5000, n_st=5000
+
+!integer :: ntt(n_sim)
+
+real*8 :: av_h(n_sim),rn0(n_sim)
+
+integer, allocatable :: nt_v(:,:)
+real*8, allocatable :: t(:,:), av_step(:),std_step(:),t_step(:)
+	
+character(len=6) :: FMT1
+character(len=6) :: FMT
+	!t_v(n_sim,n_st) total number of stators in simulation m in step n
+    !nr_v(n_sim,n_st) total number of stators in the bulk in simulation at step n
+    !av_h (n_sim) final average of each history
+    !sd_h (n_sim) final sd of each history
+    !av_step(n_st) average number of stators in each step
+     
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!     
+!Constants
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+rk0=0.00575
+alpha=3.06
+
+eMu=-0.846d0
+
+
+write(FMT1,'(F4.2)') eJ !F4.2
+write(FMT,'(F5.2)') eMu !F4.2
+
+!open(70,file='Initial_conditions_J='//FMT1//'.dat')
+
+do j=1,n_sim
+  !read(70,*) rn0(j)
+  
+  rn0(j)=6.2d0
+end do
+
+
+    
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!Trajectories calculation
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+call KMC_Traces(rk0,alpha,eMu,rn0,n_v,n_sim,n_st,nt_v,t,ntt)
+
+allocate(av_step(ntt))
+allocate(std_step(ntt))
+allocate(t_step(ntt))
+
+
+!File opening
+
+
+if(rn0(10)==0.0d0) then
+   open(50,file='Traces_Res_KMC.dat')
+else
+   open(50,file='Traces_Stall_KMC.dat')
+end if
+
+
+do i=1,ntt
+
+  !Average value and time of each step
+
+  sum_step=0.0d0
+  sum_t=0.0d0
+  
+  do j=1,n_sim
+  
+    sum_step = sum_step + nt_v(j,i)
+    sum_t  = sum_t + t(j,i)
+  
+  end do
+  
+  av_step(i) = sum_step/real(n_sim)
+  t_step(i) = sum_t/real(n_sim)
+  
+  
+  !Standard deviation of each step
+  
+  sum_sd=0.0d0
+  
+  do j=1,n_sim
+  
+    sum_sd = sum_sd + (nt_v(j,i)-av_step(i))**2.0d0
+  
+  end do
+
+  std_step(i) = dsqrt(sum_sd/real(n_sim))
+  
+  !File filling
+  
+  write(50,*) t_step(i), av_step(i), std_step(i)
+  
+end do
+
+total_av = Average(100,ntt,av_step)
+total_sd = Average(100,ntt,std_step)
+av_time = Average(1,ntt,t_step)
+
+write(*,*) '<N> = ', total_av, ' +- ', total_sd
+write(*,*) '<t> = ', av_time
+  
+
+
+
+
+!File closing
+     
+close(50)
+
+
+write(*,*)      
+write(*,*) 'Program finished'
+end 
